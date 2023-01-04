4bf9c9ef2bbb0d04d02d40b169780ef825a9ede4
boomermitra <110715433+boomermitra@users.noreply.github.com>
2023-01-04 21:47:00 +0530

    api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import AxiosApi from "./component/AxiosApi";
 
 function App() {
   return (
     <div className="App">
       <h1>Ashutosh - Checking Netlify Deployment</h1>
-      <h1>Change 1 - Done!</h1>
-      <h2>change 2- from another branch</h2>
+      <h3>Change 1 - Done!</h3>
+      <h3>change 2- from another branch</h3>
+      <h3>change 3- Axios API Call</h3>
+      <AxiosApi />
     </div>
   );
 }
---
+++ src/component/AxiosApi.js
@@ -0,0 +1,88 @@
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import { Button, Card } from "react-bootstrap";
+
+const AxiosApi = () => {
+  const [data, setData] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
+    headers: {
+      "X-RapidAPI-Key": "4baf334e09msh61f8fad670a4ed1p186bc3jsn02e67035fc8a",
+      "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+    },
+  };
+
+  useEffect(() => {
+    axios
+      .request(options)
+      .then(function (response) {
+        setData(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  if (data) console.log(data);
+
+  return (
+    <>
+      <div>
+        <h1>Free-to-Play Games Database - Using Axios</h1>
+      </div>
+      <div className="container" style={container}>
+        {data &&
+          data.map((items) => (
+            <div className="card" style={card}>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img src={items.thumbnail} width="270" />
+                <Card.Body>
+                  <Card.Title>
+                    <h3>{items.title}</h3>
+                  </Card.Title>
+                  <Card.Text>
+                    <b>
+                      <i>{items.short_description}</i>
+                    </b>
+                    <br />
+                    <br />
+                    <b>Publisher:</b> {items.publisher}
+                    <br />
+                    <b>Developer: </b> {items.developer}
+                    <br />
+                    <b>Gener: </b> {items.genre}
+                    <br />
+                  </Card.Text>
+                  <a href={items.game_url} target="_blank">
+                    <Button variant="success">More Info</Button>
+                  </a>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+
+const container = {
+  display: "flex",
+  width: "100% vw",
+  flexDirection: "row",
+  margin: "auto",
+  flexWrap: "wrap",
+  justifyContent: "space-evenly",
+};
+
+const card = {
+  display: "block",
+  margin: "10px",
+  padding: "20px",
+  backgroundColor: "#ABBAEA",
+  //   padding: "20 px",
+};
+export default AxiosApi;
