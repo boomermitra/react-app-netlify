017e33ce3fcfa853b79bc0a2ae7e3b4c96648bd9
boomermitra <110715433+boomermitra@users.noreply.github.com>
2023-01-04 21:25:49 +0530

    changes on branch 2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Ashutosh - Checking Netlify Deployment</h1>
       <h1>Change 1 - Done!</h1>
+      <h2>change 2- from another branch</h2>
     </div>
   );
 }
